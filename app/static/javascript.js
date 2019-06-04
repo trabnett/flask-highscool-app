84f39d061288acf243ceef0eda1be4bfe92e78aa
trabnett <timrabnett@gmail.com>
2019-06-04 11:16:52 -0400

    added modal for password change

--- app/static/javascript.js
+++ app/static/javascript.js
@@ -20,3 +20,27 @@ function tests(num, input){
     x.appendChild(y)
     }
 }
+
+function match(e){
+    let div = document.getElementById('new_password2')
+    if (div.childNodes[5]){
+        div.childNodes[5].remove()
+    }
+    console.log(div.childNodes, "<====")
+    let pwd1 = document.getElementById('Password2').value
+    let pwd2 = document.getElementById('Password3').value
+    if (pwd1 === "" && pwd2 === ""){
+        return
+    }
+    if (pwd1 === pwd2){
+        let warning = document.createElement('div')
+        warning.innerHTML = `<p style="text-align:center;color:green;">They Match</p>`
+        div.appendChild(warning)
+        console.log('yes sir')
+    } else {
+        let warning = document.createElement('div')
+        warning.innerHTML = `<p style="text-align:center;color:red;">Please make sure your passwords match</p>`
+        div.appendChild(warning)
+        console.log('no match')
+    }
+}
