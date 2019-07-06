72cc634f45e0d98e62bf6f871bf6e139b5dea31e
trabnett <timrabnett@gmail.com>
2019-07-06 15:44:47 -0400

    added jquery

--- app/static/javascript.js
+++ app/static/javascript.js
@@ -1,55 +1,53 @@
-//workaround for styling wtforms quickforms buttons so that they match bootstrap buttons 
-let quick_form = document.getElementById('quick_form')
-if (quick_form){
-    quick_form.childNodes.forEach(node => {
-        if (node.type === 'submit'){
-            node.classList.add('bootstrap-copy')
+
+$(document).ready(function(){
+    // adjustments for various mobile views
+    $('#school').height($(window).height() - $('.first').height()).css({'background-repeat': 'no-repeat', 'background-attachment': 'fixed', 'background-size': 'cover'}).attr("class","")
+    let offset = ($(window).height()/2) - 40
+    $('.homepage-card').css('margin-top', offset)
+    if ($(window).width() < 590){
+        $('.card-backdrop').height(1000)
+        $('.homepage-card').css('margin-top', 20)
+    } else {
+        $('.card-backdrop').height($(window).height())
+    }
+
+
+    // scroll fade feature for homepage
+    let scroll = 0
+    $(window).scroll(function(){
+        let id1 = ''
+        let id2 = ''
+        let scroll_num = 0
+        if (scroll < 644){
+            scroll_num = scroll
+            id1 = "school"
+            id2 = "class"
+        } else if (scroll < 1210) {
+            scroll_num = scroll - 800
+            id1 = "class"
+            id2 = "sportpic"
+        } else if (scroll < 2011) {
+            scroll_num = scroll - 1500
+            id1 = "sportpic"
+            id2 = "cafeteria"
+        } else {
+            scroll_num = scroll - 2080
+            id1 = "cafeteria"
+            id2 = "test"
         }
-    })
-}
+        let opacity = 1 - (scroll_num / 500)
+        scroll = $(this).scrollTop()
+        $(`#${id1}`).css('opacity', `${opacity}`)
+        $(`#${id2}`).css('opacity', `${1 - opacity}`)
+    });
 
-// scroll fade feature for homepage
-let x = 0
-function scroll(){
-    let id1 = ''
-    let id2 = ''
-    let scroll_num = 0
-    if (x < 644){
-        scroll_num = x
-        id1 = "school"
-        id2 = "class"
-    } else if (x < 1210) {
-        scroll_num = x - 800
-        id1 = "class"
-        id2 = "sportpic"
-    } else if (x < 2011) {
-        scroll_num = x - 1550
-        id1 = "sportpic"
-        id2 = "cafeteria"
-    } else {
-        scroll_num = x - 2380
-        id1 = "cafeteria"
-        id2 = "test"
-    }
-    y = document.getElementById(id1)
-    z = document.getElementById(id2)
-    let opacity = 1 - (scroll_num / 500)
-    if (window.scrollY > x) {
-        x = window.scrollY
-        y.style.opacity = opacity
-        z.style.opacity = 1 - opacity
-    } else {
-        x = window.scrollY
-        y.style.opacity = opacity
-        z.style.opacity = 1 - opacity
-    }
-    x = window.scrollY
-}
+    //workaround for styling wtforms quickforms buttons so that they match bootstrap buttons 
+    $('#quick_form').find(':submit').addClass('bootstrap-copy')
 
-if (document.getElementById('school')){
-    document.addEventListener("scroll", scroll)
-}
 
+  })
+
+  
 // display test scores on student page
 function tests(num, input){
     let x = document.getElementById(`row${num}`)
@@ -68,28 +66,16 @@ function tests(num, input){
     x.parentNode.insertBefore(node, x.nextSibling)
 }
 
-// check if password and new password match in password modal
+// check if password and new password match in settings modal
 function match(){
-    let div = document.getElementById('new_password2')
-    if (div.childNodes[5]){
-        div.childNodes[5].remove()
+    if ($('#Password2').val() === "" || $('#Password3').val() === ""){
+        return $('#password-match').html('')
     }
-    let pwd1 = document.getElementById('Password2').value
-    let pwd2 = document.getElementById('Password3').value
-    if (pwd1 === "" || pwd2 === ""){
-        return
-    }
-    if (pwd1 === pwd2){
-        let button = document.getElementById('password_update_button')
-        button.type = "submit"
-        let warning = document.createElement('div')
-        warning.innerHTML = `<p style="text-align:center;color:green;">&#10004; They Match</p>`
-        div.appendChild(warning)
+    if ($('#Password2').val() === $('#Password3').val()){
+        $('#password_update_button').prop('type', 'submit')
+        $('#password-match').html('&#10004; They Match').css('color', 'green')
     } else {
-        let button = document.getElementById('password_update_button')
-        button.type = 'button'
-        let warning = document.createElement('div')
-        warning.innerHTML = `<p class="warning">&#10007 Please make sure your passwords match</p>`
-        div.appendChild(warning)
+        $('#password_update_button').prop('type', 'button')
+        $('#password-match').html('&#10007 Please make sure your passwords match').css('color', 'red')
     }
 }
